972b60208761eafc0c27bbf74a6bfcc374a16fff
Sailant <149213148+Sailanth@users.noreply.github.com>
2026-03-13 23:25:29 -0300

    Refactor exceptions to use HTTPException for errors

--- 13 - APIs Assíncronas com FastAPI/desafio/src/exceptions.py
+++ 13 - APIs Assíncronas com FastAPI/desafio/src/exceptions.py
@@ -1,6 +1,34 @@
-class AccountNotFoundError(Exception):
-    pass
+from fastapi import HTTPException, status
 
 
-class BusinessError(Exception):
-    pass
+class UserAlreadyExistsException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Nome de usuário já está em uso.",
+        )
+
+
+class InvalidCredentialsException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciais inválidas.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+class AccountNotFoundException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Conta não encontrada.",
+        )
+
+
+class InsufficientBalanceException(HTTPException):
+    def __init__(self, balance: str):
+        super().__init__(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Saldo insuficiente. Saldo atual: R$ {balance}.",
+        )
